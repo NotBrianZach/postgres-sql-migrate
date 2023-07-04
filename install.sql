09e6bd1bbe488506f5b96fd732d4b96077d1c2b5
zach <poinkCompany@gmail.com>
2023-07-04 16:40:03 -0500

    update

--- install.sql
+++ install.sql
@@ -1,19 +1,10 @@
 DO
 $body$
 BEGIN
-  -- This file adds versioning support to database it will be loaded to.
-  -- It requires that PL/pgSQL is already loaded - will raise exception otherwise.
-  -- All versioning "stuff" (tables, functions) is in "_v" schema.
-
   CREATE SCHEMA IF NOT EXISTS _v;
   COMMENT ON SCHEMA _v IS 'Schema for versioning data and functionality.';
 
   IF NOT EXISTS (SELECT FROM pg_catalog.pg_tables WHERE tablename = 'patches') THEN
-    -- CREATE TABLE applied_migrations (
-    --   id TEXT NOT NULL PRIMARY KEY
-    --   , ddl TEXT NOT NULL
-    --   , applied_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
-    -- );
 
     CREATE TABLE _v.rollbacked_patches (
       patch_name TEXT NOT NULL PRIMARY KEY,
@@ -21,6 +12,8 @@ BEGIN
       rollback_ddl TEXT NOT NULL,
       rollbacked_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
     );
+
+    -- if a patch is found in this table, it should already be applied to the database
     CREATE TABLE _v.patches (
       patch_name TEXT NOT NULL PRIMARY KEY,
       ddl TEXT NOT NULL,
@@ -56,41 +49,47 @@ BEGIN
           var_conflict TEXT;
     BEGIN
       LOCK TABLE _v.patches IN EXCLUSIVE MODE;
+
       IF
-        NOT EXISTS (SELECT 1 FROM _v.patches p WHERE p.patch_name = in_patch_name)
-          AND
-        NOT EXISTS (SELECT 1 FROM _v.patch_conflicts c WHERE c.patch_name = in_patch_name)
+        EXISTS (SELECT 1 FROM _v.patches p WHERE p.patch_name = in_patch_name)
       THEN
-        IF conflicts IS NOT NULL THEN
-          IF EXISTS (SELECT 1 FROM _v.patches p WHERE p.patch_name = ANY(conflicts)) THEN
-            RAISE NOTICE 'Patch % already applied', in_patch_name;
-            RETURN FALSE;
-          END IF;
-
-          foreach var_conflict IN ARRAY conflicts LOOP
-            INSERT INTO _v.patch_conflicts (patch_name, conflict_name) VALUES (in_patch_name, var_conflict);
-          END LOOP;
-        END IF;
-
-        IF dependencies IS NOT NULL THEN
-          foreach var_dep IN ARRAY dependencies LOOP
-             INSERT INTO _v.patch_deps (patch_name, depend_name) VALUES (in_patch_name, var_dep);
-             IF NOT EXISTS (SELECT 1 FROM _v.patches p WHERE p.patch_name = var_dep) THEN
-               RAISE NOTICE 'Applying previously rollbacked dependant patch: %', var_dep;
-               SELECT ddl INTO var_dep_ddl FROM _v.rollbacked_patches WHERE patch_name = var_dep;
-               EXECUTE var_dep_ddl;
-             END IF;
-          END LOOP;
-        END IF;
-
-        RAISE NOTICE 'Applying patch: %', in_patch_name;
-        EXECUTE in_ddl;
-
-        INSERT INTO _v.patches (patch_name, ddl, rollback_ddl) VALUES (in_patch_name, in_ddl, in_rollback_ddl);
-
-        RETURN TRUE;
+        RAISE EXCEPTION 'Patch % already applied', in_patch_name;
       END IF;
-      RETURN FALSE;
+
+      IF conflicts IS NOT NULL THEN
+        SELECT c.conflict_name into var_conflict FROM _v.patch_conflicts c WHERE p.patch_name = ANY(c.conflict_name) limit 1;
+        IF EXISTS () THEN
+          RAISE EXCEPTION 'Conflicting patch % ', var_conflict;
+        END IF;
+
+        foreach var_conflict IN ARRAY conflicts LOOP
+          INSERT INTO _v.patch_conflicts (patch_name, conflict_name) VALUES (in_patch_name, var_conflict);
+        END LOOP;
+      END IF;
+
+      IF dependencies IS NOT NULL THEN
+        foreach var_dep IN ARRAY dependencies LOOP
+          IF NOT EXISTS (SELECT 1 FROM _v.patches p WHERE p.patch_name = var_dep) THEN
+            RAISE NOTICE 'Checking rollbacked patches for unapplied patch dependency: %', var_dep;
+            IF NOT EXISTS (SELECT 1 FROM _v.rollback_patches r WHERE r.patch_name = var_dep) THEN
+              RAISE EXCEPTION 'No rollback patch found for unnaplied patch dependency %, need to apply manually', var_dep;
+            END IF;
+            SELECT ddl INTO var_dep_ddl FROM _v.rollbacked_patches WHERE patch_name = var_dep;
+            RAISE NOTICE 'Rollbacked patch found; applying patch dependency: %', var_dep;
+            EXECUTE var_dep_ddl;
+            RAISE NOTICE 'Deleting relevant entry from rollback table: %', var_dep;
+            DELETE FROM _v.rollbacked_patches WHERE patch_name = var_dep;
+          END IF;
+          INSERT INTO _v.patch_deps (patch_name, depend_name) VALUES (in_patch_name, var_dep);
+        END LOOP;
+      END IF;
+
+      RAISE NOTICE 'Applying patch: %', in_patch_name;
+      EXECUTE in_ddl;
+
+      INSERT INTO _v.patches (patch_name, ddl, rollback_ddl) VALUES (in_patch_name, in_ddl, in_rollback_ddl);
+
+      RETURN TRUE;
     END;
     $$ LANGUAGE plpgsql;
   END IF;
@@ -106,10 +105,11 @@ BEGIN
           SELECT depend_name FROM _v.patch_deps WHERE depend_name = in_patch_name;
       BEGIN
         LOCK TABLE _v.rollback_patches IN EXCLUSIVE MODE;
+
         SELECT ddl,rollback_ddl INTO var_ddl,var_rollback_ddl FROM _v.patches WHERE patch_name = in_patch_name;
 
         IF NOT FOUND THEN
-          RAISE EXCEPTION 'Patch % not found', in_patch_name;
+          RAISE EXCEPTION 'Rollback ddl for patch % not found', in_patch_name;
         END IF;
 
         -- Retrieve dependent patches
